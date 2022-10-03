c675c99af66efde7647df939e32c9bbaecfa400c
kamwal <57108044+kamwal@users.noreply.github.com>
2022-10-03 11:27:41 +0100

    deleting old files

--- PCM-design/PCM_utils_forDMQC/__init__.py
+++
@@ -1,7 +0,0 @@
-#
-
-from .data_fetcher import get_refdata, add_floatdata 
-from .data_processing import interpolate_standard_levels, get_regulargrid_dataset
-from .Plotter import Plotter
-from .Plotter import plot_spatialdist_ds
-from .BIC_calculation import BIC_calculation, plot_BIC
